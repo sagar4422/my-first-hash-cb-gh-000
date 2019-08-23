f1646d4b17042f75a7c0f60a53697dde673b40dd
Sagar Gururaj Deshpande <sagargururaj.d@gmail.com>
2019-08-23 11:04:56 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -16,7 +16,7 @@ def shipping_manifest
   # We have 5 whale bone corsets, 2 porcelain vases and 3 oil paintings
    the_manifest = {
      "whale bone corsets" => 5,
-     "porcelain" => 2,
+     "porcelain vases" => 2,
      "oli paintings" => 3
    }
 
