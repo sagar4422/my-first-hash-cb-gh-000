e54cd7468b2fd0baa4b5c46823bf58cae51180f1
Sagar Gururaj Deshpande <sagargururaj.d@gmail.com>
2019-08-23 11:01:42 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,6 +1,11 @@
 def my_hash
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
-
+  my_hash = {
+    "name" => "sagar",
+    "country" => "India"
+  }
+  my_hash
+  return my_hash
   
 end
 
@@ -8,8 +13,15 @@ end
 def shipping_manifest
   # set a variable called `the_manifest`, equal to a hash
   # fill that hash with key/value pairs that describe the following information: 
-  # We have 5 whale bone corsets, 2 porcelain vases and 3 oil paintings 
+  # We have 5 whale bone corsets, 2 porcelain vases and 3 oil paintings
+   the_manifest = {
+     "whale bone corsets" => 5,
+     "porcelain" => 2,
+     "oli paintings" => 3
+   }
 
+   the_manifest
+   return the_manifest
 
 end
 
@@ -21,7 +33,9 @@ def retrieval
                  }
 
   # Look up the value of the "oil paintings" key in the shipping_manifest hash below
-
+  val = shipping_manifest["oil paintings"]
+  val
+  return val
 end
 
 def adding
@@ -31,13 +45,15 @@ def adding
                   "oil paintings" => 3
                  }
   # add 2 muskets to the shipping_manifest hash below
-
+  shipping_manifest["muskets"] = 2
 
 
   # add 4 gun powder to the shipping_manifest hash below
-
+  shipping_manifest["gun powder"] = 4
 
 
   # return the shipping_manifest hash below
+  shipping_manifest
+  return shipping_manifest
 
 end
